018fb04eab0ab8e09891b71a55ed82e0f9e9f088
System Administrator <root@Imperio.local>
2019-11-23 01:15:01 -0500

    payment and order

---
+++ controllers/ordersController.js
@@ -0,0 +1,65 @@
+var Q = require('q');
+var OrderService = require('../services/orderService');
+
+module.exports.controller = function(app) {
+    app.route('/orders')
+    .all(function(req, res, next) {
+        next();
+    })
+    .get(function(req, res) {
+        OrderService.findOrders()
+        .then(function(orders) {
+            res.json(orders);
+        })
+        .catch(function(err) {
+            console.log(err);
+            res.status(500).json(err);
+        });
+    })
+    .post(function(req, res) {
+        OrderService.createOrder(req.body)
+        .then(function(order) {
+            res.json(order);
+        })
+        .catch(function(err) {
+            console.log(err);
+            res.status(500).json(err);
+        });
+    });
+
+    app.route('/order/:id')
+    .all(function(req, res, next) {
+        next();
+    })
+    .get(function(req, res) {
+        OrderService.findOrderById(req.params.id)
+        .then(function(order) {
+            res.json(order);
+        })
+        .catch(function(err) {
+            console.log(err);
+            res.status(500).json(err);
+        });
+    })
+    .put(function(req, res) {
+        OrderService.updateOrder(req.params.id, req.body)
+        .then(function(obj) {
+            res.json(obj);
+        })
+        .catch(function(err) {
+            console.log(err);
+            res.json(err);
+        });
+    })
+    .delete(function(req, res) {
+        OrderService.deleteOrder(req.params.id)
+        .then(function(obj) {
+            res.json(obj);
+        })
+        .catch(function(err) {
+            console.log(err);
+            res.json(err);
+        });
+    });
+
+};
---
+++ controllers/paymentsController.js
@@ -0,0 +1,65 @@
+var Q = require('q');
+var PaymentService = require('../services/paymentService');
+
+module.exports.controller = function(app) {
+    app.route('/orders')
+    .all(function(req, res, next) {
+        next();
+    })
+    .get(function(req, res) {
+        PaymentService.findPayments()
+        .then(function(payments) {
+            res.json(payments);
+        })
+        .catch(function(err) {
+            console.log(err);
+            res.status(500).json(err);
+        });
+    })
+    .post(function(req, res) {
+        PaymentService.createPayment(req.body)
+        .then(function(payment) {
+            res.json(payment);
+        })
+        .catch(function(err) {
+            console.log(err);
+            res.status(500).json(err);
+        });
+    });
+
+    app.route('/order/:id')
+    .all(function(req, res, next) {
+        next();
+    })
+    .get(function(req, res) {
+        PaymentService.findPaymentById(req.params.id)
+        .then(function(payment) {
+            res.json(payment);
+        })
+        .catch(function(err) {
+            console.log(err);
+            res.status(500).json(err);
+        });
+    })
+    .put(function(req, res) {
+        PaymentService.updatePayment(req.params.id, req.body)
+        .then(function(obj) {
+            res.json(obj);
+        })
+        .catch(function(err) {
+            console.log(err);
+            res.json(err);
+        });
+    })
+    .delete(function(req, res) {
+        PaymentService.deletePayment(req.params.id)
+        .then(function(obj) {
+            res.json(obj);
+        })
+        .catch(function(err) {
+            console.log(err);
+            res.json(err);
+        });
+    });
+
+};
